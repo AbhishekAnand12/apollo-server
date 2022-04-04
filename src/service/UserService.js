977668e73e3b378067644b3e260e4bda6ca65a0f
Abhishek Anand <abhishek.anand@successive.tech>
2022-04-04 15:29:47 +0530

    modification in userService

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,27 +1,41 @@
+import PubSub from "../modules/pubsub";
 class userService {
   constructor() {
     this.id = 0;
     this.trainee = new Map();
   }
+  
   createTraineeData({ name, email }) {
-    this.trainee.set(this.id, { id: this.id, name, email });
-    const newTrainee = this.trainee.get(this.id);
     this.id++;
+    this.trainee.set(this.id, { id: this.id, name, email });
+    PubSub.publish("TRAINEE_CREATED_WITH_SUBSCRIBE", {
+      createTraineeData: { id: this.id, name, email },
+    });
+    const newTrainee = this.trainee.get(this.id);
     return newTrainee;
   }
 
   updateTraineeData({ id, name, email }) {
     this.trainee.delete(parseInt(id));
     this.trainee.set(parseInt(id), { id, name, email });
+    PubSub.publish("TRAINEE_UPDATED_WITH_SUBSCRIBE", {
+      updateTraineeData: { id: this.id, ...this.trainee }
+    });
     const updatedTrainee = this.trainee.get(parseInt(id), { id, name, email });
     return updatedTrainee;
   }
 
-  deleteTraineeData = ({ id }) => this.trainee.delete(parseInt(id));
-
   getAllTraineeData() {
     return this.trainee.values();
   }
+
+  deleteTraineeData({ id }) {
+    PubSub.publish("TRAINEE_DELETED_WITH_SUBSCRIBE", {
+      deleteTraineeData: { id: id }
+    });
+    return this.trainee.delete(parseInt(id));
+  }
+
 }
 
 export default new userService();
