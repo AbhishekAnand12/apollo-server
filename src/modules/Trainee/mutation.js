e7f72d59821425b6eba0dc95addd675422d1de0f
Abhishek Anand <abhishek.anand@successive.tech>
2022-04-05 18:26:30 +0530

    PUBLISH in mutation

--- src/modules/Trainee/mutation.js
+++ src/modules/Trainee/mutation.js
@@ -1,9 +1,31 @@
 import userService from "../../service/UserService";
+import {TRAINEE_CREATED_WITH_SUBSCRIBE,
+TRAINEE_UPDATED_WITH_SUBSCRIBE,
+TRAINEE_DELETED_WITH_SUBSCRIBE} from '../../lib/constant';
+import PubSub from "../pubsub";
+
 
 export default {
-  createTraineeData: (_, { input }) => userService.createTraineeData(input),
+  createTraineeData: (_, { input }) => {
+    PubSub.publish(TRAINEE_CREATED_WITH_SUBSCRIBE, {
+      createTraineeData: userService.createTraineeData(input),
+    });
+    return userService.createTraineeData(input);
+  },
+  updateTraineeData: (_, { input }) => {
+    PubSub.publish(TRAINEE_UPDATED_WITH_SUBSCRIBE, {
+      updateTraineeData: userService.updateTraineeData(input),
+    });
+    return userService.updateTraineeData(input);
+  },
+  deleteTraineeData: (_, { input }) => {
+    PubSub.publish(TRAINEE_DELETED_WITH_SUBSCRIBE, {
+      deleteTraineeData: userService.deleteTraineeData(input),
+    });
+    return userService.deleteTraineeData(input);
+  },
 
-  updateTraineeData: (_, { input }) => userService.updateTraineeData(input),
+  // updateTraineeData: (_, { input }) => userService.updateTraineeData(input),
 
-  deleteTraineeData: (_, { input }) => userService.deleteTraineeData(input),
+  // deleteTraineeData: (_, { input }) => userService.deleteTraineeData(input),
 };
--- src/modules/Trainee/subscription.js
+++ src/modules/Trainee/subscription.js
@@ -5,9 +5,11 @@ export default {
     subscribe: () => Pubsub.asyncIterator("TRAINEE_CREATED_WITH_SUBSCRIBE"),
   },
   updateTraineeData: {
-    subscribe: () => Pubsub.asyncIterator("TRAINEE_UPDATED_WITH_SUBSCRIBE"),
+    subscribe: () => {
+      return Pubsub.asyncIterator("TRAINEE_UPDATED_WITH_SUBSCRIBE");
+    },
   },
-  updateTraineeData: {
+  deleteTraineeData: {
     subscribe: () => Pubsub.asyncIterator("TRAINEE_DELETED_WITH_SUBSCRIBE"),
   },
 };
